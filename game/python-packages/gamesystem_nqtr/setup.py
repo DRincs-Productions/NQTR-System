3bb3fd18ce8a053b4f3b8f5180a3ae6a96c02071
Exodia <67595890+DonRP@users.noreply.github.com>
2023-01-27 19:45:57 +0100

    inizial

---
+++ game/python-packages/gamesystem_nqtr/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nqtrlib',
+    packages=find_packages(include=['nqtrlib']),
+    version='1.0.2',
+    description='Navigation Quest Time Routine System',
+    author='DRincs',
+    license='MIT',
+)
