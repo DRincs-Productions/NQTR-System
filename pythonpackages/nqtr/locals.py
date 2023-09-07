fff49fff69934e5ac1ef882d0d19f3f4ac657d20
Black Ram <67595890+DonRP@users.noreply.github.com>
2023-09-07 18:47:13 +0200

    format

--- pythonpackages/nqtr/locals.py
+++ pythonpackages/nqtr/locals.py
@@ -1,13 +1,21 @@
 from pythonpackages.nqtr.action import Act, clear_expired_actions, current_actions
 from pythonpackages.nqtr.action_talk import TalkObject
 from pythonpackages.nqtr.button import Button
-from pythonpackages.nqtr.navigation import (Location, Map, Room, get_room_by_id,
-                                            is_closed_room)
+from pythonpackages.nqtr.navigation import (
+    Location,
+    Map,
+    Room,
+    get_room_by_id,
+    is_closed_room,
+)
 from pythonpackages.nqtr.quest import Quest, Stage
-from pythonpackages.nqtr.routine import (Commitment, clear_expired_routine,
-                                         commitment_background,
-                                         characters_commitment_in_current_location,
-                                         characters_events_in_current_location)
+from pythonpackages.nqtr.routine import (
+    Commitment,
+    clear_expired_routine,
+    commitment_background,
+    characters_commitment_in_current_location,
+    characters_events_in_current_location,
+)
 from pythonpackages.nqtr.time import TimeHandler
 
 __all__ = [
